7569df928292824bac08663b5d91c2e9d48aa15d
ANANTHSSA <ananthssa2000@gmail.com>
2023-02-06 16:28:51 +0530

    update

---
+++ js/min.js
@@ -0,0 +1,294 @@
+let car={
+					model:"car",
+					number: 4567,
+					color:"black",
+					company: "rolls rayos",
+					year: "100%",
+					speed: "2000km/hr",
+				}
+				document.write(JSON.stringify(car));
+				console.log(car);
+				console.log(car.company);
+				console.log(car["speed"]);
+				document.write("<br>");
+				document.write("<br>");
+				let world={
+					world:"night mode",
+					color: "multi",
+					size: "round",
+					people: 12390599-46,
+					man: 3458067,
+					woman: 750286,
+				}
+				document.write(JSON.stringify(world));
+				console.log(world);
+				console.log(world.people);
+				console.log(world["man"]);
+				document.write("<br>");
+				document.write("<br>");
+				let bag={
+					color:"red",
+					type:"collage",
+					zip: 5,
+					weight: 500,
+					length: 700,
+					strong:"1molebower", 
+				}
+				document.write(JSON.stringify(bag));
+				console.log(bag);
+				console.log(bag.zip);
+				console.log(bag["type"]);
+				document.write("<br>");
+				document.write("<br>");
+				let byCicle={
+					color: "black",
+					model: 123,
+					speed: "40km/hr",
+					look: "super",
+					break: "super",
+					number: 4783,
+					
+				}
+
+				document.write(JSON.stringify(byCicle));
+				console.log(byCicle);
+				console.log(byCicle.speed);
+				console.log(byCicle["color"]);
+				document.write("<br>");
+				document.write("<br>");
+				let frendent={
+					html: "completed",
+					css: "completed",
+					bootsrap: "completed",
+					month: 4,
+					next: "job",
+					javascript: 25,
+					
+				}
+				document.write(JSON.stringify(frendent));
+				console.log(frendent);
+				console.log(frendent.next);
+				console.log(frendent["css"]);
+				document.write("<br>");
+				document.write("<br>");
+				 var ananth={
+			  model:"fashion",
+			  size:"big",
+			  type:3,
+			  chip:5,
+			  cloth:"very thick",
+			}
+			document.write(JSON.stringify(ananth));
+			console.log(ananth);
+			console.log(ananth.cloth);
+			console.log(ananth["size"]);
+			document.write("<br>");
+			document.write("<br>");
+				let nd ={
+					mnumber:1,
+					name:"swift",
+					number:2829,
+					model:2021,
+					company:"Honda",
+					color:"red",
+					type:"four wheeler"
+					}
+				console.log(nd.company);
+				console.log(nd["color"]);
+				console.log(nd);
+				document.write(JSON.stringify(nd));
+				document.write("<br>");
+				document.write("<br>");
+				let animal={
+					number:3,
+					category:"cat",
+					type:"whild animal",
+					color:"ash",
+					price:2000,
+					name:"pri",
+					brand:"teddy",
+				}
+				console.log(animal.color);
+				console.log(animal["name"]);
+				console.log(animal);
+				document.write(JSON.stringify(animal));	
+				document.write("<br>");
+				document.write("<br>");
+				let mobile={
+					number:4,
+					model:"note 7 pro",
+					type:"android",
+					color:"blue",
+					price:20000,
+					year:2017,
+					brand:"redmi"
+				}	
+				console.log(mobile.type);
+				console.log(mobile["price"]);
+				console.log(mobile);
+				document.write(JSON.stringify(mobile));
+				document.write("<br>");
+				document.write("<br>");
+				let bike={
+					number:5,
+					model:"pulsar",
+					speed:120,
+					color:"black",
+					price:20000,
+					year:2017,
+					brand:"honda"
+				}	
+				console.log(bike.model);
+				console.log(bike["price"]);
+				console.log(bike);
+				document.write(JSON.stringify(bike));
+				document.write("<br>");
+				document.write("<br>");
+				let toys={
+					number:6,
+					category:"kitchenset",
+					items:2,
+					user:"child",
+					color:"pink",
+					price:200,
+					brand:"aphm"
+				}	
+				console.log(toys.category);
+				console.log(toys["brand"]);
+				console.log(toys);
+				document.write(JSON.stringify(toys));
+				document.write("<br>");
+				document.write("<br>");
+				<!----telivision---->
+			let tv={
+				name:"tv",
+				model:"sony",
+				feturs:"connect your mobile",
+				color:"black",
+				price:35000,
+				manufacture:"bombay",
+			}
+			document.write(JSON.stringify(tv));
+			console.log(tv);
+			console.log(tv.model);
+			console.log(tv["feturs"]);
+			document.write("<br>");
+			document.write("<br>");
+			<!----movie---->
+			let movie={
+				name:"movie",
+				title:"thalapathy67",
+				relesd:"diwali",
+				date:"nov 4",
+				hero:"thalapathy",
+				director:"lokesh",
+			}
+			document.write(JSON.stringify(movie));
+			console.log(movie);
+			console.log(movie.title);
+			console.log(movie["director"]);
+			document.write("<br>");
+			document.write("<br>");
+			<!----bike-------->
+			let bikes={
+				brandname:"yamaha",
+				model:"rx 100",
+				lanched:1978,
+				color:"black",
+				feturs:"double strok",
+				highspeed:120,
+			}
+			document.write(JSON.stringify(bikes));
+			console.log(bikes);
+			console.log(bikes.model);
+			console.log(bikes["highspeed"]);
+			document.write("<br>");
+			document.write("<br>");
+			<!----teacher-------->
+			let teacher={
+				name:"anitha",
+				gender:"female",
+				age:35,
+				exprince:"5years",
+				subject:"english",
+				salary:15000,
+			}
+			document.write(JSON.stringify(teacher));
+			console.log(teacher);
+			console.log(teacher.subject);
+			console.log(teacher["name"]);
+			document.write("<br>");
+			document.write("<br>");
+			<!----fan---->
+			let fan={
+				brandname:"panasonic",
+				model:"hifi",
+				type:"table fan",
+				color:"white",
+				price:1800,
+				speed:6,
+			}
+			document.write(JSON.stringify(fan));
+			console.log(fan);
+			console.log(fan.type);
+			console.log(fan["brandname"]);
+			document.write("<br>");
+			document.write("<br>");
+			<!----bus---->
+			let bus={
+				name:"mini bus",
+				model:"mahendra",
+				lanched:2008,
+				color:"green",
+				price:300000,
+				manufacture:"chenni",
+			}
+			document.write(JSON.stringify(bus));
+			console.log(bus);
+			console.log(bus.model);
+			console.log(bus["color"]);
+			document.write("<br>");
+			document.write("<br>");
+			<!----charger---->
+			let charger={
+				name:"charger",
+				usage:"charging",
+				type:"c",
+				volte:64,
+				price:850,
+				speed:"1hr fully chrging",
+			}
+			document.write(JSON.stringify(charger));
+			console.log(charger);
+			console.log(charger.usage);
+			console.log(charger["speed"]);
+			document.write("<br>");
+			document.write("<br>");
+			let hollywood={
+				name:"tom&jerry",
+				characters:"car&mouse",
+				type:"comedy entertainment",
+				fav:"children",
+				programme:"cn",
+				myfavone:"mouse",
+			}
+			document.write(JSON.stringify(hollywood));
+			console.log(hollywood);
+			console.log(hollywood.characters);
+			console.log(hollywood["programme"]);
+			document.write("<br>");
+			document.write("<br>");
+			// let fan={
+				// name:"usha",
+				// usage:"mist of duos",
+				// type:"short & long",
+				// air flow:"blades",
+				// price:1200,
+				// speed:"value switch in 5",
+			// }
+			// document.write(JSON.stringify(fan));
+			// console.log(fan);
+			// console.log(fan.usage);
+			// console.log(fan["price"]);
+			// document.write("<br>");
+			// document.write("<br>");
